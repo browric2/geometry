6624082532e8a7b769b3439fd73d026d895610c2
browric2 <richbrown1995@hotmail.co.uk>
2020-02-18 00:28:27 +0000

    Done for the night, not working properly yet.

--- geom_fit.py
+++ geom_fit.py
@@ -2,11 +2,13 @@ from PIL import Image
 import numpy as np
 from scipy.optimize import curve_fit
 import pickle as pkl
-
+import pygame
 
 xsize = 50
 ysize = 50
+size = (xsize,ysize)
 
+pygame.init()
 
 
 im = Image.open('sunset edit 4.png')
@@ -20,17 +22,52 @@ yvals = arf
 xvals = np.arange(len(yvals))
 
 
+screen = pygame.display.set_mode(size)#,pygame.FULLSCREEN)
+pygame.display.set_caption("Geometrize")
+clock = pygame.time.Clock()
 
-def f(x,screen,left,top,width,height,R,G,B):
+currentshapes = []
 
-    x.reshape(ysize,xsize,3)
+def f(x,left,top,width,height,R,G,B):
 
-    y = [0] * int(len(x)/3)
-    for i in range(int(len(x)/3)):
-        y[i] = np.array([R,G,B])
+    npix = int(len(x)/3)
+    x_rs = x.reshape(ysize,xsize,3)
+    y = [0] * npix
+
+    for i in range(npix):
+        xpos = (i % xsize)
+        ypos = int(np.floor(i % ysize))
+
+        if left <= xpos < (left+width) and top <= ypos < (top+height):
+            y[i] = np.array([R,G,B])
+        else:
+            y[i] = np.array(screen.get_at((xpos,ypos))[:3])
+
 
     return np.array(y).flatten()
 
+done = False
+
+j = 1
+while not done:
+    print(j)
+    j += 1
+
+    screen.fill((255,255,255))
+
+    for cs in currentshapes:
+        print(cs[1])
+        pygame.draw.rect(screen, cs[1], cs[0])
+
+    q,r = curve_fit(f,xvals,yvals)
+
+    l,t,w,h = int(q[0]),int(q[1]),int(q[2]),int(q[3])
+    cRed,cGreen,cBlue = int(q[4]),int(q[5]),int(q[6])
+
+    currentshapes.append((pygame.Rect(l,t,w,h),(cRed,cGreen,cBlue)))
+
+    clock.tick(60)
+    pygame.display.flip()
 
 
 
